31e5da4d710d146368c1de58c33823f15747d973
Luighi <luighiavz@gmail.com>
2021-02-20 20:06:14 -0500

    Add q flag to apt

--- scada-aws/install-docker.sh
+++ scada-aws/install-docker.sh
@@ -5,7 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
 sudo apt-get clean
 sudo apt-get update
-sudo apt-get -o Dpkg::Options::="--force-confdef" upgrade -y
+sudo apt-get -o Dpkg::Options::="--force-confdef" upgrade -yq
 sudo apt-get install -y  apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
